7f6f3cd452ad4b2955974873a30ff52c7d5f8035
Meldi <meldi.sayoko@pinhome.id>
2021-03-25 22:28:12 +0700

    initial commmit

---
+++ src/main/java/propensi/d06/sihedes/SihedesApplication.java
@@ -0,0 +1,13 @@
+package propensi.d06.sihedes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SihedesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SihedesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/propensi/d06/sihedes/SihedesApplicationTests.java
@@ -0,0 +1,13 @@
+package propensi.d06.sihedes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SihedesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
