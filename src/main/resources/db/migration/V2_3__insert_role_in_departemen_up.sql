59834036daf69dbbfd2246e0c546c56087fc3711
Meldi <meldi.sayoko@pinhome.id>
2021-04-13 15:03:56 +0700

    feat:add migration file

---
+++ src/main/resources/db/migration/V2_3__insert_role_in_departemen_up.sql
@@ -0,0 +1,8 @@
+INSERT INTO sihedes.departemen(id_dept, nama_departemen, nama_kepala) VALUES(1, "IT Helpdesk", "Earlene");
+INSERT INTO sihedes.departemen(id_dept, nama_departemen, nama_kepala) VALUES(2, "Security", "Praya");
+INSERT INTO sihedes.departemen(id_dept, nama_departemen, nama_kepala) VALUES(3, "IT Sys Dev", "Meldi");
+INSERT INTO sihedes.departemen(id_dept, nama_departemen, nama_kepala) VALUES(4, "IT User Admin", "Bela");
+INSERT INTO sihedes.departemen(id_dept, nama_departemen, nama_kepala) VALUES(5, "IT Network", "Ajeng");
+INSERT INTO sihedes.departemen(id_dept, nama_departemen, nama_kepala) VALUES(6, "IT Infrastructure", "Nindya");
+INSERT INTO sihedes.departemen(id_dept, nama_departemen, nama_kepala) VALUES(7, "Finance", "Aku");
+INSERT INTO sihedes.departemen(id_dept, nama_departemen, nama_kepala) VALUES(8, "HR", "Kamu");
---
+++ src/main/resources/db/migration/V2_4__insert_status_down.sql
@@ -0,0 +1,1 @@
+DELETE FROM sihedes.status;
