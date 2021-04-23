06231933cfa483fb74752fb7e99dcdfe93c741a5
Syanne Limarwan <syannelimarwan@gmail.com>
2021-04-23 15:10:42 +0700

    assign resolver

--- src/main/java/propensi/d06/sihedes/controller/TicketController.java
+++ src/main/java/propensi/d06/sihedes/controller/TicketController.java
@@ -1,22 +1,24 @@
 package propensi.d06.sihedes.controller;
-import propensi.d06.sihedes.model.ProblemModel;
-import propensi.d06.sihedes.model.RequestModel;
+
+import propensi.d06.sihedes.model.*;
+import propensi.d06.sihedes.service.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
 public class TicketController {
-
-//    @Autowired
-//    private ProblemService problemService;
-//
-//    @Autowired
-//    private RequestService requestService;
+    @Autowired
+    private ProblemService problemService;
+    
+    @Autowired
+    private RequestService requestService;
 
     @GetMapping("/tickets")
     public String listTickets(
@@ -55,9 +57,34 @@ public class TicketController {
 //        model.addAttribute("hasHotel", hasHotel);
 //
 //        model.addAttribute("listHotel", listHotel);
-//        // Return view template yang diinginkan
+    //    Return view template yang diinginkan
+            // if (problem.getStatus().getNama_status() == "open"){
+            //     return "detailProblem";
+            // }
+            // else if (problem.getStatus().getNama_status() == "Waiting for Assignment"){
+            //     return "assignResolverProblem";
+            // }
         return "detailProblem";
     }
+
+    @GetMapping("/problem/resolver")
+    public String detailResolverProblem(
+            @ModelAttribute ProblemModel problem,
+            Model model) {
+
+        return "assignResolverProblem";
+    }
+
+    // @PostMapping("/problem/detail")
+    // public String ResolveProblem(
+    //     @RequestParam(value = "jenisResolver") Long id,
+    //     @ModelAttribute ProblemModel problem,
+    //     RedirectAttributes redir) {
+    //     problem.setResolver_departemen(problemService.getDepById(id));
+    //     problemService.updateProblem(problem);
+    //     return "allTickets";
+    // }
+
 
     @GetMapping("/request/detail")
     public String detailRequest(
@@ -76,6 +103,35 @@ public class TicketController {
 //
 //        model.addAttribute("listHotel", listHotel);
 //        // Return view template yang diinginkan
+        // if (request.getStatus().getNama_status() == "open"){
+        //         return "detailRequest";
+        //     }
+        // else if (request.getStatus().getNama_status() == "Waiting for Assignment"){
+        //     return "assignResolverRequest";
+        // }
         return "detailRequest";
+    }
+
+    @GetMapping("/request/resolver")
+    public String detailResolveRequest(
+            @ModelAttribute RequestModel request,
+            Model model) {
+
+        return "assignResolverRequest";
+    }
+
+    @PostMapping("/request/resolver")
+    public String ResolveRequest(
+        @RequestParam(value = "jenisResolver") Long id,
+        @ModelAttribute RequestModel request,
+        RedirectAttributes redir) {
+        if (id == 0){
+            redir.addFlashAttribute("gagal", "Resolver Departemen belum dipilih!");
+            return "redirect:/request/resolver"; 
+        } else {
+            request.setResolver_departemen(requestService.getDepById(id));
+            requestService.updateRequest(request);
+            return "allTickets";
+        }
     }
 }
--- src/main/resources/db/migration/V2_1__insert_role_in_role_up.sql
+++ src/main/resources/db/migration/V2_1__insert_role_in_role_up.sql
@@ -1,35 +1,35 @@
-# DROP TABLE IF EXISTS sihedes.role;
-#
-# CREATE TABLE IF NOT EXISTS sihedes.role (
-#   id_role bigint(20)  NOT NULL,
-#   nama_role varchar(50) NOT NULL
-# );
-#
-# --
-# -- Dumping data for table role
-# --
-#
-# INSERT INTO sihedes.role (id_role, nama_role) VALUES
-# (1, 'Admin'),
-# (2, 'Helpdesk'),
-# (3, 'Pengaju'),
-# (4, 'Kepala Departemen IT'),
-# (5, 'Staff IT'),
-# (6, 'Kepala Departemen');
-#
-# --
-# -- Indexes for table role
-# --
-# ALTER TABLE sihedes.role
-#     ADD PRIMARY KEY (id_role);
-#
-# --
-# -- AUTO_INCREMENT for dumped tables
-# --
-#
-# --
-# -- AUTO_INCREMENT for table role
-# --
-# ALTER TABLE sihedes.role
-#     MODIFY id_role bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-# COMMIT;
+-- # DROP TABLE IF EXISTS sihedes.role;
+-- #
+-- # CREATE TABLE IF NOT EXISTS sihedes.role (
+-- #   id_role bigint(20)  NOT NULL,
+-- #   nama_role varchar(50) NOT NULL
+-- # );
+-- #
+-- # --
+-- # -- Dumping data for table role
+-- # --
+-- #
+-- # INSERT INTO sihedes.role (id_role, nama_role) VALUES
+-- # (1, 'Admin'),
+-- # (2, 'Helpdesk'),
+-- # (3, 'Pengaju'),
+-- # (4, 'Kepala Departemen IT'),
+-- # (5, 'Staff IT'),
+-- # (6, 'Kepala Departemen');
+-- #
+-- # --
+-- # -- Indexes for table role
+-- # --
+-- # ALTER TABLE sihedes.role
+-- #     ADD PRIMARY KEY (id_role);
+-- #
+-- # --
+-- # -- AUTO_INCREMENT for dumped tables
+-- # --
+-- #
+-- # --
+-- # -- AUTO_INCREMENT for table role
+-- # --
+-- # ALTER TABLE sihedes.role
+-- #     MODIFY id_role bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+-- # COMMIT;
