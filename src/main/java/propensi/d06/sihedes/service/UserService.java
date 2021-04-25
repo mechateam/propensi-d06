b24a826dcc3904278761cbde2e4e7b21932173e1
prayatj <prayatjondro@gmail.com>
2021-04-25 11:49:18 +0700

    feat/dashboard layouting

--- src/main/java/propensi/d06/sihedes/service/UserService.java
+++ src/main/java/propensi/d06/sihedes/service/UserService.java
@@ -9,10 +9,7 @@ public interface UserService {
     UserModel addUser(UserModel user);
     String encrypt(String password);
     UserModel getUserbyUsername(String username);
-<<<<<<< HEAD
-    List<UserModel> getListUserbyDepartemen(DepartemenModel departemen);
-=======
     UserModel getUserbyId(Long id);
->>>>>>> 3f774e20f97fa871367dc845719b82e1aa480505
     boolean isMatch(String newPassword, String oldPassword);
+    List<UserModel> getListUserbyDepartemen(DepartemenModel departemen);
 }
